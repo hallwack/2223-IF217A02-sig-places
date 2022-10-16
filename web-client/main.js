b9a0267528a1fce10aa72723ff48bc32d05d7f3a
Raihan Adam <hallwack.id@gmail.com>
2022-10-16 21:03:04 +0700

    fix: fixing placesLayer to show polygon with db connection

--- web-client/main.js
+++ web-client/main.js
@@ -1,70 +1,65 @@
-import './style.css';
-import {Map, View} from 'ol';
-import TileLayer from 'ol/layer/Tile';
-import OSM from 'ol/source/OSM';
-import XYZ from 'ol/source/XYZ';
-import VectorSource from 'ol/source/Vector';
-import VectorLayer from 'ol/layer/Vector';
-import GeoJSON from 'ol/format/GeoJSON';
+import "./style.css";
+import { Map, View } from "ol";
+import TileLayer from "ol/layer/Tile";
+import OSM from "ol/source/OSM";
+import XYZ from "ol/source/XYZ";
+import VectorSource from "ol/source/Vector";
+import VectorLayer from "ol/layer/Vector";
+import GeoJSON from "ol/format/GeoJSON";
 
-const placesGeoJSON = {
-  "type": "FeatureCollection",
-  "features": [
+/* const placesGeoJSON = {
+  type: "FeatureCollection",
+  features: [
     {
-      "type": "Feature",
-      "geometry": {
-        "type": "Point",
-        "coordinates": [107.7177, -6.9254]
+      type: "Feature",
+      geometry: {
+        type: "Point",
+        coordinates: [107.7177, -6.9254],
       },
-      "properties": {
-        "name": "UIN Sunan Gunung Djati",
-        "address": "Jalan AH Nasution"
+      properties: {
+        name: "UIN Sunan Gunung Djati",
+        address: "Jalan AH Nasution",
       },
     },
-{
-      "type": "Feature",
-      "geometry": {
-        "type": "Point",
-        "coordinates": [107.7277, -6.9254]
+    {
+      type: "Feature",
+      geometry: {
+        type: "Point",
+        coordinates: [107.7277, -6.9254],
       },
-      "properties": {
-        "name": "Apotik",
-        "address": "Jalan AH Nasution"
+      properties: {
+        name: "Apotik",
+        address: "Jalan AH Nasution",
       },
-    }
-  ] 
-}
+    },
+  ],
+}; */
 
 let placesLayer = new VectorLayer({
-  source: new VectorSource({
-    features: new GeoJSON().readFeatures(placesGeoJSON),
-  })
+  source: new VectorSource(),
 });
 
-fetch("http://localhost:3100/places?format=geojson").then(
-  (response) => {
-    return response.json()
-  }
-).then(
-  (jsonResponse) => {
-
-    console.log("jsonResponse nya", jsonResponse)
+fetch("http://127.0.0.1:3100/places?format=geojson")
+  .then((response) => {
+    return response.json();
+  })
+  .then((jsonResponse) => {
+    console.log("jsonResponse nya", jsonResponse);
     placesLayer.setSource(
       new VectorSource({
-        features: new GeoJSON().readFeatures(jsonResponse),
+        features: new GeoJSON().readFeatures(jsonResponse.data),
       })
-    )
-  }
-)
+    );
+  });
 
 const map = new Map({
-  target: 'map',
+  target: "map",
   layers: [
-    new TileLayer({
+    /* new TileLayer({
       source: new XYZ({
-        url: `https://mt0.google.com/vt/lyrs=s&hl=en&x={x}&y={y}&z={z}`
-      })
-    }),
+        url: `https://mt0.google.com/vt/lyrs=s&hl=en&x={x}&y={y}&z={z}`,
+      }),
+    }), */
     new TileLayer({
       source: new OSM(),
     }),
@@ -73,6 +68,6 @@ const map = new Map({
   view: new View({
     projection: `EPSG:4326`,
     center: [107.7177, -6.9254],
-    zoom: 13, 
-  })
+    zoom: 13,
+  }),
 });
